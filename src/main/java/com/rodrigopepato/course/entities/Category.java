f471bed976364d13a6775b5f2ad96e5d7a1410e9
rodrigopepato <rodrigo.pepato@gmail.com>
2025-09-29 21:55:57 -0300

    Many to Many association with JoinTable

--- src/main/java/com/rodrigopepato/course/entities/Category.java
+++ src/main/java/com/rodrigopepato/course/entities/Category.java
@@ -1,9 +1,11 @@
 package com.rodrigopepato.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 import java.io.Serializable;
@@ -20,6 +22,8 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
