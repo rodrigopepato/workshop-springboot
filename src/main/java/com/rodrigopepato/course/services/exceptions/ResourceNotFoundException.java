c96d0dc67e0df668b36220ddfefc98109dd0e9b2
rodrigopepato <rodrigo.pepato@gmail.com>
2025-10-01 15:35:38 -0300

    Exception handling - findById

---
+++ src/main/java/com/rodrigopepato/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.rodrigopepato.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
